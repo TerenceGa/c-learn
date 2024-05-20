9dd8f9a858b52164e1ceebeb0a30f3b50048e2ce
ter21 <qgan1219@bu.edu>
2024-05-20 17:45:52 -0400

    c++ first step

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-# c-learn
+#include <cstdio>
+void print(char const* myString)
+{
+    printf("String %s\n", myString);
+}
+
+void print(int myInt)
+{
+    printf("My int is %d\n", myInt);
+}
+int main()
+{
+    print("Hello"); // Resolves to void print(const char*)
+    print(15); // Resolves to void print(int)
+}
